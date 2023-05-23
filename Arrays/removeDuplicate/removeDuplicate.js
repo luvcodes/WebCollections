81ec1a7be7768e8368286e5c8148ce2889db3111
Runze Yang <ryan0028@student.monash.edu>
2023-05-23 23:08:15 +1000

    remove duplicate elements in array

---
+++ Arrays/removeDuplicate/removeDuplicate.js
@@ -0,0 +1,18 @@
+/**
+ * 数组去重 要求去除数组中重复的元素
+ * 1. 目标: 把旧数组里面不重复的元素选取出来放到新数组中，重复的元素只保留一个，放到新数组中去重。
+ * 2. 核心算法: 我们遍历旧数组，拿着就数组元素去查询新数组，如果该元素在新数组里面没有出现过，我们就添加，否则不添加。
+ * 3. 我们怎么知道该元素没有存在？利用 新数组.indexOf(数组元素) 如果返回时 -1 就说明 新数组里面没有该元素
+ * 封装一个 去重的函数 unique 独一无二的
+ */
+function unique(arr) {
+    var newArr = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (newArr.indexOf(arr[i]) === -1) {
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
+}
+var arr = ['c', 'a', 'z', 'a', 'x', 'a', 'x', 'c', 'b'];
+console.log(unique(arr));
