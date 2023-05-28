cec718d0d9296ba8f501a10a718df77e55dfc673
ryan0028 <ryan0028@student.monash.edu>
2023-05-28 16:23:45 +1000

    Update for project structure

--- PhaseTwo/DOM/PracticeExample/showPic.js
+++ PhaseTwo/DOM/PracticeExample/showPic.js
@@ -1,16 +1,79 @@
+function addLoadEvent(func) {
+    var oladonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function () {
+            oladonload();
+            func();
+        }
+    }
+}
+
+function insertAfter(newElement, targetElement) {
+    var parent = targetElement.parentNode;
+    if (parent.lastChild == targetElement) {
+        parent.appendChild(newElement);
+    } else {
+        parent.insertBefore(newElement, targetElement.nextSibling);
+    }
+}
+
+function preparePlaceholder() {
+    if (!document.createElement) return false;
+    if (!document.createTextNode) return false;
+    if (!document.getElementById) return false;
+    if (!document.getElementById("imagegallery")) return false;
+    var placeholder = document.createElement("img");
+    placeholder.setAttribute("id", "placeholder");
+    placeholder.setAttribute("src", "C:\Users\ryanw\OneDrive\Desktop\壁纸\wallhaven-zm9kpy.jpeg");
+    placeholder.setAttribute("alt", "my image gallery");
+    var description = document.createElement("p");
+    description.setAttribute("id", "description");
+    var desctext = document.createTextNode("Choose an image");
+    description.appendChild(desctext);
+    var gallery = document.getElementById("imagegallery");
+    insertAfter(placeholder, gallery);
+    insertAfter(description, placeholder);
+}
+
+function prepareGallery() {
+    if (!document.getElementsByTagName) return false;
+    if (!document.getElementById) return false;
+    if (!document.getElementById("imagegallery")) return false;
+    var gallery = document.getElementById("imagegallery");
+    var links = gallery.getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+        // 下面的意思是 当这个链接被点击时，showPic()函数就开始执行
+        links[i].onclick = function () {
+            return showPic(this);
+        }
+        links[i].onkeypress = links[i].onclick;
+    }
+}
+
+/**
+ * 两个功能: 
+ * 1. 找出id属性值是placeholder的图片并修改其src属性
+ * 2. 找出id属性是description的元素并修改其第一个子元素的nodeValue属性，其实也就是修改图片说明文字(title)
+ */
 function showPic(whichpic) {
+    if (!document.getElementById("placeholder")) return true;
     var source = whichpic.getAttribute("href");
     var placeholder = document.getElementById("placeholder");
     placeholder.setAttribute("src", source); // placeholder.src = source;
-
-    var text = whichpic.getAttribute("title");
+    if (!document.getElementById("description")) return false;
+    if (whichpic.getAttribute("title")) {
+        var text = whichpic.getAttribute("title");
+    } else {
+        var text = "";
+    }
     var description = document.getElementById("description");
-    description.firstChild.nodeValue = text;
+    if (description.firstChild.nodeType == 3) {
+        description.firstChild.nodeValue = text;
+    }
+    return false;
 }
 
-// function countBodyChildren() {
-//     var body_element = document.getElementsByTagName("body")[0];
-//     // alert(body_element.childNodes.length);
-//     alert(body_element.nodeType);
-// }
-// window.onload = countBodyChildren;
+addLoadEvent(preparePlaceholder);
+addLoadEvent(prepareGallery);
