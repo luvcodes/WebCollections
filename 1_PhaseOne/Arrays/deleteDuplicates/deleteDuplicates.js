c939bf63a4ed682246a3a8fca274b15346fad6ab
ryan0028 <ryan0028@student.monash.edu>
2023-06-30 15:11:39 +1000

    update full folder structure

--- 1_PhaseOne/Arrays/deleteDuplicates/deleteDuplicates.js
+++ 1_PhaseOne/Arrays/deleteDuplicates/deleteDuplicates.js
@@ -6,7 +6,7 @@
 var arr = [2,0,66,1,77,0,52,0,25,7];
 var newArr = [];
 for (var i = 0; i < arr.length; i++) {
-    if (arr[i] != 0) {
+    if (arr[i] !== 0) {
         newArr[newArr.length] = arr[i];
     }
 }
